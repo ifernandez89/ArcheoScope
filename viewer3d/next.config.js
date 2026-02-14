d1f85e7e592ee9ee8fb2837cfaadae76b1baa3f4
Ignacio <nacho.xiphos@gmail.com>
2026-02-13 23:28:03 -0300

    fix: Restaurar configuración que funcionaba en Pages

--- viewer3d/next.config.js
+++ viewer3d/next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig = {
   output: 'export',
-  basePath: isGitHubPages ? '/ArcheoScope' : '',
-  assetPrefix: isGitHubPages ? '/ArcheoScope/' : '',
+  basePath: isProd ? '/ArcheoScope' : '',
+  assetPrefix: isProd ? '/ArcheoScope/' : '',
   reactStrictMode: true,
   images: {
     unoptimized: true,
